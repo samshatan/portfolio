ed8c2edd1b44343e26e1cd79c4c08d8d684b1553
Sameer Maurya <sameermaurya97044@gmail.com>
2026-02-15 19:43:10 +0530

    feat: Initialize new React project with Vite, Tailwind CSS, and GitHub Pages deployment.

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
